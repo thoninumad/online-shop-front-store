8847d5c66b68638eff9cdf42cd524dd411d4a561
Syafrie Dwi Faisal <syafriedwif@gmail.com>
2019-04-05 02:30:16 +0700

    Update Front About

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,7 +1,69 @@
 <template>
-    <div class="about">
-        <v-container>
-<<<<<<< HEAD
+    <div class="main about">
+            <v-img
+              src="/img/about_image.png"
+              max-height="300"
+              class="grey darken-4"
+            ></v-img>
+
+            <v-container style="margin-top:-180px;"> 
+                <v-card elevation="5">
+                    <h1 class="text-md-center black--text font-weight-regular" style="padding:100px 0 50px;font-size:50px;">CERITA KAMI</h1>
+                    <v-container>
+                    <v-layout justify-center align-center style="margin:40px 5px 0 0;">
+                        <v-flex xs6>
+                            <img src="https://i.imgur.com/kYJlWYQ.png" style="display:block;margin: 30px auto;" alt="Logo ITS" width="45%" />
+                            <h1 style="text-align: center;"><span style="color: #336799;"><strong>Science</strong></span></h1>
+                            <h1 style="text-align: center;"><span style="color: #336799;"><strong>Technopark</strong></span></h1>
+                            <h1 style="text-align: center;"><span style="color: #336799;"><strong>Otomotif</strong></span></h1>
+                        </v-flex>
+                        <v-flex xs6>
+                            <h2 style="padding-bottom:40px;color:#bd1544;">Dibalik semua cerita</h2>
+                            <p>&emsp;STP ITS adalah lembaga yang mendukung inovasi dan komersialisasi teknologi, pengembangan kreasi usaha dan lapangan kerja dan pengembangan ekonomi dari hasil hilirisasi riset oleh dosen dan mahasiswa.&nbsp; </p> <p> STP memiliki tujuh fokus,
+                                 diantaranya industri otomotif, kemaritiman, industri kreatif , Permukiman dan lingkungan, ICT dan Nano teknologi yang tergabung dalam Science Technopark (STP). STP di ITS berfungsi menjembatani&nbsp; riset perguruan tinggi dengan dunia industri. Di dalam STP, riset
+                                  yang dijalankan di ITS akan dikembangkan dan ditumbuhkan dalam bentuk perusahaan pemula berbasis teknologi (spin off). </p> <p> Sehingga, terbentuk ekosistem inovasi dengan aktor utama quadruple helix, yakni akademik, bisnis, pemerintah dan masyarakat. Program otomotif-its
+                                   sendiri merupakan salah satu kegiatan yang dibina oleh STP bidang otomotif. Dalam hal ini STP Otomotif bertindak sebagai penyandang dana dan induk kelembagaan.</p>      
+                        </v-flex>
+                    </v-layout>
+                    <v-layout justify-center align-center style="margin:100px 0 0 30px;">
+                        <v-flex xs6>
+                            <h2 style="padding-bottom:40px;color:#bd1544;">Awal Cerita</h2>
+                            <p>&emsp;Departemen Teknik Mesin ITS berada di bawah Fakultas Teknologi Industri ITS. Demi menjalankan misi yaitu &lsquo;Menyelenggarakan program Pendidikan Teknik Mesin untuk menghasilkan lulusan yang mampu bersaing di pasar global,
+                                 melaksanakan penelitian dan mempublikasikan hasil penelitian bidang keilmuan Teknik Mesin yang berkualitas dalam forum nasional, regional maupun internasional, serta menerapkan hasil penelitian untuk membantu memecahkan masalah
+                                  industri dan masyarakat&rsquo;,</p><p> pada tahun 2010 telah dicanangkan bahwa pengembangan pendidikan, penelitian dan pengabdian kepada masyarakat dilakukan dengan lebih memberdayakan dan mengedepankan peran laboratorium sesuai dengan konsep lab-based education.
+                                   Departemen Teknik Mesin ITS dalam program otomotif-its ini bertindak sebagai penyandang dana bersama-sama dengan STP Otomotif ITS namun tidak menjadi induk struktural.</p>
+                        </v-flex>
+                        <v-flex xs6>
+                            <img src="https://i.imgur.com/NT10FF1.png" style="display:block;margin: 30px auto;" alt="Logo ITS" width="45%" />      
+                        </v-flex>
+                    </v-layout>
+                    <v-layout justify-center align-center style="margin:100px 30px 0 0;">                        
+                        <v-flex xs6>
+                            <img src="https://i.imgur.com/pUfkoDT.png" style="display:block;margin: 30px auto;" alt="Logo ITS" width="45%" />
+                        </v-flex>
+                        <v-flex xs6>
+                            <h2 style="padding-bottom:40px;color:#bd1544;">Wajah Kami</h2>
+                            <p>&emsp; Lembaga Bengkel Mahasiswa Mesin ITS (LBMM ITS) adalah lembaga yang didirikan pada tanggal 27 Mei 1990 di Departemen Teknik Mesin ITS oleh sejumlah mahasiswa yang mempunyai hobi dan minat dalam bidang otomotif. Lembaga Bengkel Mahasiswa Mesin sendiri bergerak dalam bidang PENELITIAN, PEMBINAAN, PENGEMBANGAN, DAN PELAYANAN MASYARAKAT dalam bidang otomotif. LBMM ITS pada program otomotif-its ini berperan sebagai eksekutif untuk seluruh kegiatan dalam cakupan otomotif-its.</p>      
+                        </v-flex>
+                    </v-layout>
+                    <v-layout justify-center align-center style="margin:150px 0 80px;">
+                        <v-flex xs12>                        
+                            <p style="text-align:center;"><strong>XENDIT</strong></p>
+                            <p style="text-align:center;"><a href="https://xendit.github.io/xendit-dashboard-docs/invoices.html">https://xendit.github.io/xendit-dashboard-docs/invoices.html</a></p>
+                            <p style="text-align:center;">no rek Bni 0605280435 an roze windu Yuni Syarah</p>
+                        </v-flex>
+                    </v-layout>
+
+                    
+                </v-container>
+                </v-card>
+
+                
+            </v-container>
+
+
+
+            <!--
             <div class="hero-image"> </div>
             <h1 style="text-align: center;">&nbsp;</h1>
             <h1 style="text-align: center;">&nbsp;</h1>
@@ -22,10 +84,6 @@
             <p><a href="https://xendit.github.io/xendit-dashboard-docs/invoices.html">https://xendit.github.io/xendit-dashboard-docs/invoices.html</a></p>
             <p>no rek Bni 0605280435 an roze windu Yuni Syarah</p>
             <p style="text-align: justify;">&nbsp;</p>
-            <p>&nbsp;</p>
-=======
-            <h3>This is an about page</h3>
->>>>>>> 9c721a65e71b103eaf2b2e24464437010525f073
-        </v-container>
+            <p>&nbsp;</p>-->
     </div>
 </template>
